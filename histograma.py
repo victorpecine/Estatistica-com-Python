4986e8508b8840d2c842061f09decd274bcdc815
Victor Pecine <victorpecine@gmail.com>
2021-05-29 15:21:44 -0300

    Histograma

---
+++ histograma.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+dados = pd.read_csv('dados.csv')
+
+# histograma com seaborn
+# ax = sns.histplot(dados.Altura, kde=False)
+
+# ax.figure.set_size_inches(12, 6)
+# ax.set_title('Distribuição de Frequências - Altura', fontsize=18)
+# ax.set_xlabel('Metros (m)', fontsize=14)
+# plt.show()
+
+# histograma com plandas
+# dados.Altura.hist(bins=50, figsize=(12, 6))
+# plt.show()
+
