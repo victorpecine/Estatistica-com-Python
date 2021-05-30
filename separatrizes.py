83f89eb0a9946590f2427b4c0cfde48a7b8b0fd3
Victor Pecine <victorpecine@gmail.com>
2021-05-30 14:46:40 -0300

    Cálculo dos quartis, decis e percentis

---
+++ separatrizes.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dados = pd.read_csv('dados.csv')
+
+mediana_rendas = dados.Renda.quantile()
+
+quartis_renda = dados.Renda.quantile([0.25, 0.5, 0.75])
+
+decis_renda = dados.Renda.quantile([i / 10 for i in range(1, 10)])
+
+percentis_renda = dados.Renda.quantile([i / 100 for i in range(1, 100)])
+
+ax = sns.distplot(dados.Idade,
+                  hist_kws={'cumulative': True},
+                  kde_kws={'cumulative': True})
+ax.figure.set_size_inches(14, 6)
+ax.set_title('Distribuição de Frequências Acumuladas', fontsize=18)
+ax.set_ylabel('Acumulado', fontsize=14)
+ax.set_xlabel('Anos', fontsize=14)
+plt.show()
